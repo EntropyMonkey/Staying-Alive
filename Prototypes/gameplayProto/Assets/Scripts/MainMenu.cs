af2d3f44702a3ce31c064372e950d1b148c32bf5
attacksnack <schnack.morten@gmail.com>
2012-12-05 13:02:57 +0100

    Updated main menu

--- Prototypes/gameplayProto/Assets/Scripts/MainMenu.cs
+++ Prototypes/gameplayProto/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,9 @@ public class MainMenu : MonoBehaviour {
 
     // Code inspired by http://forum.unity3d.com/threads/36866-How-to-make-a-main-menu-in-Unity
 
-    public int buttonType = 0; // 1 = play, 2 = quit, 3 = settings
+    public int buttonType = 0; // different types, do different things
+
+    public GameObject camera;
 
     void OnMouseEnter()
     {
@@ -19,17 +21,48 @@ public class MainMenu : MonoBehaviour {
 
     void OnMouseDown()
     {
-        if (buttonType == 1)
+        switch(buttonType)
         {
-            // start playing the game
-            Debug.Log("Loading level!");
-            Application.LoadLevel(0);
-        }
-        else if (buttonType == 2)
-        {
-            // Quit the game
-            Debug.Log("Exiting!");
-            Application.Quit();
+            case 1:
+                // new game: change camera to show The Escape or The Troll Cave
+                Vector3 v = new Vector3();
+                v.x = 0.2f;
+                v.y = 8;
+                camera.transform.Translate(v);
+                break;
+            case 2:
+                // the escape : start playing the first level
+                Debug.Log("Loading first level!");
+                Application.LoadLevel(0);
+                break;
+            case 3:
+                // the troll cave : start playing the second level
+                Debug.Log("Loading second level!");
+                Application.LoadLevel(1); // WARNING: This may not set the currentLevel variable in the Player correctly!
+                break;
+            case 4:
+                // go back to main menu
+                Debug.Log("Going back to main menu");
+                Vector3 v2 = new Vector3();
+                v2.x = -0.2f;
+                v2.y = -8;
+                camera.transform.Translate(v2);
+                break;
+            case 5:
+                // config
+                Debug.Log("Config - hasn't been implemented yet");
+                break;
+            case 6:
+                // credits
+                Debug.Log("Credits - hasn't been implemented yet");
+                break;
+            case 7:
+                // Quit the game
+                Debug.Log("Exiting!");
+                Application.Quit();
+                break;
+            default:
+                break;
         }
     }
 }
